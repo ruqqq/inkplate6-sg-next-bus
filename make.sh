e83a2f7c791e405b6a794311e82751ce6ff1ecbe
Faruq Rasid <me@ruqqq.sg>
2022-07-16 11:13:54 +0800

    Fix build scripts

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 GOOS=linux GOARCH=arm GOARM=7 go build -o inkplate6-sg-next-bus
 
 docker run --rm -ti --name hassio-builder --privileged \
-  -v .:/data \
+  -v `pwd`:/data \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   homeassistant/amd64-builder -t /data --all --test \
   -i inkplate6-sg-next-bus-{arch} -d ruqqq
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker push ruqqq/inkplate6-sg-next-bus-armv7:latest
-docker push ruqqq/inkplate6-sg-next-bus-armv7:${jq --raw-output '.version' config.json}
+docker push ruqqq/inkplate6-sg-next-bus-armv7:`jq --raw-output '.version' config.json`
